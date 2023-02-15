7745dcd754bfd35764f4f0b5e38560c7bc565902
Patrick5432 <maxx5326@gmail.com>
2023-02-16 00:35:35 +0500

    Конец игры про сейф. Нужно исправить баги и добавить некоторые фичи

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 'use strict';
+alert(`Добро пожаловать в игру GameCodeSelection! В этой игре вам предстоит подобрать код от сейфа, который постоянно меняется, после каждого перезапуска страницы!
+
+Удачи!
+`)
+alert(`
+Уточнение:
+ -При подборе числа в определённой области, происходит одно из четырёх действий:
+    1. "Далеко" - число довольно далеко от правильной комбинации.
+    2. "Тепло" - число стоит возле комбинации на расстоянии 2 чисел.
+    3. "Горячо" - число стоит возле комбинации на расстоянии 1 числа.
+    4. "Щёлк" - число совпало с комбинацией.
+`)
 
 function creatCombination() {
     const combination = ['', '', '']
     let result = 0
 
     for (let i = 0; i < combination.length; i++) {
-        result = (Math.floor(Math.random() * 10))
-
-        if (result === 0) {
-            combination[i] = result + 1
-        }
-
-        else {
-            combination[i] = result
-        }
+        combination[i] = String(Math.floor(Math.random() * 10))
     }
     return combination
 }
@@ -22,6 +26,7 @@ function creatCombination() {
 
 function printCombination() {
     let orderNumber = ''
+    
     const selectionCombination = ['', '', '']
     for (let j = 0; j < selectionCombination.length; j++) {
         if (j === 0) {
@@ -36,13 +41,18 @@ function printCombination() {
             orderNumber = 'третье'
         }
 
-        selectionCombination[j] = +prompt(`Введите ${orderNumber} однозначное число`)
+        selectionCombination[j] = prompt(`Введите ${orderNumber} число от 0 до 9`)
         if (selectionCombination[j] > 9) {
-            alert('Вы ввели не однозначное число!')
-            j = j - 1
+            alert('Вы вышли за предел!')
+            j--
         }
 
-        else if (selectionCombination[j] === 0) {
+        else if (selectionCombination[j] === '' || selectionCombination[j] === ' ') {
+            alert('Не может быть пустых значений')
+            j--
+        }
+
+        else if (selectionCombination[j] === null) {
             return alert('Отменено')
         }
     }
@@ -50,19 +60,45 @@ function printCombination() {
 }
 
 
-//Недоделанная функция
+
 let combination = creatCombination()
 
 function checkCombination () {
     console.log(combination)
+    let question = ''
+    let j = 0
     let userCombination = printCombination()
     console.log(userCombination)
-    
-    if (combination[0] === userCombination[0]) {
-        return alert('Вы открыли сейф!')
+
+    for (let i = 0; i < combination.length; i++) {
+        if (+combination[i] + 2 === +userCombination[i] || +combination[i] - 2 === +userCombination[i]) {
+            alert(`Замок ${i + 1}: Тепло`)
+        }
+
+        else if (+combination[i] + 1 === +userCombination[i] || +combination[i] - 1 === +userCombination[i]) {
+            alert(`Замок ${i + 1}: Горячо`)
+        }
+
+        else if (+combination[i] + 2 < +userCombination[i] || +combination[i] - 2 > +userCombination[i]) {
+            alert(`Замок ${i + 1}: Далеко`)
+        }
+
+        else {
+            alert(`Замок ${i + 1}: *Щёлк*`)
+            j++
+        }
     }
-    else if (userCombination[0] === 0) {
-        return
+    if (j === combination.length) {
+       return alert('Вы открыли сейф!').break
     }
+
+    else {
+        question = confirm('Попробовать ещё раз?')
+        if (question === true) {
+            checkCombination()
+        }
+        return alert('Вы не смогли открыть сейф').break
+    }
+
 }
 checkCombination()
